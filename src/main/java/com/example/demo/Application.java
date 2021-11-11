4dac143901c3606c50721bedc93d6697eeb88598
dntjd7701 <dntjd7701@gmail.com>
2021-11-11 12:40:41 +0900

    ##

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
 
@@ -12,11 +14,60 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+
+    // Update 이후 Configuration에서의 Bean 등록 뿐 아니라 일반 클래스에서의 Bean 등록이 가능해짐.
+    // Return 에서 바로 실행 혹은 함수 호출 가능
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
         return args -> {
-            Student Lee = new Student("Kang", "woosung", "spring@study", 27);
-            studentRepository.save(Lee);
+            Student kang = new Student(
+                    "Kang",
+                    "woosung",
+                    "spring@study",
+                    27);
+
+            Student maria = new Student(
+                    "Maria",
+                    "Jones",
+                    "maria.jones@amigoscode.edu",
+                    21
+            );
+
+            Student maria2 = new Student(
+                    "Maria",
+                    "Jones",
+                    "maria2.jones@amigoscode.edu",
+                    25
+            );
+
+            Student ahmed = new Student(
+                    "Ahmed",
+                    "Ali",
+                    "ahmed.ali@amigoscode.edu",
+                    18
+            );
+
+            // List의 형태로 저장
+            studentRepository.saveAll(List.of(kang, maria, maria2, ahmed));
+
+            studentRepository
+                    .findStudentByEmail("spring@study")
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with emailspring@study not found"));
+
+            studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqual(
+                    "Maria",
+                    21
+            ).forEach(System.out::println);
+
+            studentRepository.selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
+                    "Maria",
+                    21
+            ).forEach(System.out::println);
+
+            System.out.println("Deleting Ahmed");
+            System.out.println(studentRepository.deleteStudentById(4L));
         };
     }
 
--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -1,6 +1,36 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("select s from Student s where s.email=?1")
+    Optional<Student> findStudentByEmail(String email);
+
+    @Query("select s from Student s where s.firstName=?1 and s.age >= ?2")
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqual(String firstName, Integer age);
+
+
+    @Query(value = "select s from Student s where s.firstName=:firstName and s.age >= :age")
+    List<Student> selectStudentWhereFirstNameAndAgeGreaterOrEqualNative(
+            @Param("firstName") String firstName,
+            @Param("age") Integer age);
+
+    //@Modifying은  @Query Annotation으로 작성 된 변경, 삭제 쿼리 메서드를 사용할 때 필요합니다.
+    // 즉, 조회 쿼리를 제외하고, 데이터에 변경이 일어나는 INSERT, UPDATE, DELETE, DDL 에서 사용합니다. 주로 벌크 연산 시에 사용됩니다.
+    //(기본적으로 제공되는 쿼리 메서드나 메서드 네이밍으로 파생되는 쿼리 메서드에서는 적용이 되지 않습니다.)
+    @Transactional(rollbackFor = Exception.class)
+    @Modifying
+    @Query("DELETE FROM Student u WHERE u.id = ?1")
+    int deleteStudentById(Long id);
+
 }
