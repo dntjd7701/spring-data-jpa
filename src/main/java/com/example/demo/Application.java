98a496aaff6d2da77e1c4b463442b3984f1e95d3
dntjd7701 <dntjd7701@gmail.com>
2021-11-12 22:23:11 +0900

    #ADD session2

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import com.github.javafaker.Faker;
 
 @SpringBootApplication
 public class Application {
@@ -13,11 +14,52 @@ public class Application {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+    CommandLineRunner commandLineRunner(
+            StudentRepository studentRepository,
+            StudentIdCardRepository studentIdCardRepository) {
         return args -> {
-            Student Lee = new Student("Kang", "woosung", "spring@study", 27);
-            studentRepository.save(Lee);
+//            Faker faker = new Faker();
+//
+//            String firstName = faker.name().firstName();
+//            String lastName = faker.name().lastName();
+//            String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+//            Student student = new Student(
+//                    firstName,
+//                    lastName,
+//                    email,
+//                    faker.number().numberBetween(17, 55));
+//
+//            StudentIdCard studentIdCard = new StudentIdCard(
+//                    "123456789",
+//                    student);
+//
+//            studentIdCardRepository.save(studentIdCard);
+//
+//            studentRepository.findById(1L)
+//                    .ifPresent(System.out::println);
+//
+//            studentIdCardRepository.findById(1L)
+//                    .ifPresent(System.out::println);
+//
+//            studentRepository.deleteById(1L);
+            generateRandomStudents(studentRepository);
+
         };
+    }
+
+    private void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 55));
+            studentRepository.save(student);
+        }
     }
 
 }
